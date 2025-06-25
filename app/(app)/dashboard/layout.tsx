7496cb31e5e7fa048418438ee9bf3d759ab22fa9
Ángel Uriel González Urrutia <115201033+UriProtocol@users.noreply.github.com>
2025-06-25 15:03:47 -0600

    go back button

--- app/(app)/dashboard/layout.tsx
+++ app/(app)/dashboard/layout.tsx
@@ -6,10 +6,11 @@ import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
 import AuthWrapper from './authWrapper'
-import { FaClipboardList } from 'react-icons/fa6'
+import { FaChevronLeft, FaClipboardList } from 'react-icons/fa6'
 import CountsButton from '@/components/nav/CountsButton'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+    
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
     const menuItems = [
@@ -27,21 +28,29 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         localStorage.removeItem('token')
         router.push('/login')
     }
+    function handleGoBack(){
+        router.back()
+    }
 
     return (
         <AuthWrapper>
             <div className='h-screen w-full'>
 
                 <Navbar className='bg-[#140408]/80 backdrop-blur-xl shadow' isMenuOpen={isMenuOpen}>
-                    <NavbarContent>
+                    <NavbarContent justify='start' className='flex gap-4 justify-start'>
                         <NavbarMenuToggle
                             aria-label={isMenuOpen ? "Close menu" : "Open menu"}
                             className=""
                             onClick={() => setIsMenuOpen(!isMenuOpen)}
                         />
-                        <NavbarBrand className=' cursor-pointer' as={Link} href={'/dashboard'}>
+                        <NavbarItem className=' cursor-pointer' as={Link} href={'/dashboard'}>
                             <p className="font-bold text-inherit">V & C</p>
-                        </NavbarBrand>
+                        </NavbarItem>
+                        <NavbarItem>
+                            <Button isIconOnly className=' bg-opacity-20 rounded-full' onPress={handleGoBack}>
+                                <FaChevronLeft className=' text-xl text-primary'/>
+                            </Button>
+                        </NavbarItem>
                     </NavbarContent>
                     <NavbarContent justify="end">
                         {/*<ThemeSwitch />*/}
